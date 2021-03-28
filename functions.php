8b6f529cfb7b758a6ec41c83bcb11b41615875a5
Thiago Senna <trsenna@gmail.com>
2021-03-28 11:41:08 -0300

    a refactoring for a new child theme starter done

--- functions.php
+++ functions.php
@@ -1,19 +1,27 @@
 <?php
 
-define( 'APP_THEME', true );
-define( 'APP_THEME_FILE', __FILE__ );
+use Dalen\Theme;
 
-# ------------------------------------------------------------------------------
-# Lorem ipsum dolor sit amet
-# ------------------------------------------------------------------------------
-#
-# Suspendisse sodales ipsum non justo imperdiet, ut lacinia erat
-# cursus. Vestibulum dictum nisi ligula, in dictum justo pulvinar quis.
-#
+define('APP_THEME', true);
+define('APP_THEME_FILE', __FILE__);
 
-add_action( 'mezu/bootstrap', function ( \Dalen\theme $theme ) {
+/*  -----------------------------------------------------------------------------------------------
+    LOREM IPSUM
+    Suspendisse sodales ipsum non justo imperdiet cursus.
+--------------------------------------------------------------------------------------------------- */
 
-    $theme->register( new \App\Theme\Assets() );
-    $theme->register( new \App\Theme\Cleanup() );
+add_filter('mezu/autoload/files', function (array $files) {
+    return array_merge($files, [
+        get_theme_file_path('includes/functions-assets.php'),
+        get_theme_file_path('includes/functions-setup.php'),
+    ]);
+});
 
-} );
+/*  -----------------------------------------------------------------------------------------------
+    LOREM IPSUM
+    Suspendisse sodales ipsum non justo imperdiet cursus.
+--------------------------------------------------------------------------------------------------- */
+
+add_action('mezu/bootstrap', function (Theme $theme) {
+    // ...
+});
